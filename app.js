f7c9b97941887b4ee5eda35b6d77fd5e55d9e5c9
Ankit Singh <ankit6480@gmail.com>
2018-03-07 22:26:27 +0530

    heroku

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
 const dbRoutes = require('./routes/db-routes');
+const path= require('path');
 
 var app=express();
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine','ejs');
+
+
 var port=3000||process.env.PORT;
 
 
 
-app.use(dbRoutes);
+app.get('/',(req,res)=>{
+  res.render('index');
+});
 
 app.listen(port, () => {
     console.log('app now listening for requests on port 3000');
