1b8d7fb86e329c572f523933d96566e93997cd41
Ankit Singh <ankit6480@gmail.com>
2018-03-07 22:33:13 +0530

    heroku

--- app.js
+++ app.js
@@ -12,11 +12,7 @@ app.set('view engine','ejs');
 
 var port=process.env.PORT||3000;
 
-
-
-app.get('/',(req,res)=>{
-  res.render('index');
-});
+app.use(dbRoutes);
 
 app.listen(port,process.env.IP,()=>{
   console.log(`Starting at port ${port}`);
