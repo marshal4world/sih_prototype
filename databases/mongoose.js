4bd859c1fc11fb00e6c455f22e5dda3f7bdac003
Ankit Singh <ankit6480@gmail.com>
2018-03-08 22:29:52 +0530

    deploying on aws

--- databases/mongoose.js
+++ databases/mongoose.js
@@ -1,4 +1,9 @@
 var mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/Rivers');
+mongoose.connect(keys.mongodb.dbURI||'mongodb://localhost:27017/Rivers');
+
+
 module.exports=mongoose;
